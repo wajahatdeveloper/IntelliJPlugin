2eb63dfe36f6fc5147281ad027df185febf408c6
GitHub Action <action@github.com>
2021-06-01 09:57:00 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/wajahatdeveloper/intellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.wajahatdeveloper.intellijplugin.services
+
+import com.github.wajahatdeveloper.intellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
